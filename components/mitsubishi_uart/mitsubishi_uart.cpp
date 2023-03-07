995d1d3711b3c72079a57ce7341a7e05a6eabd03
Sammy1Am <467704+Sammy1Am@users.noreply.github.com>
2023-03-07 13:20:36 -0800

    Send and receive connect packet. Some Packet refactoring.

--- components/mitsubishi_uart/mitsubishi_uart.cpp
+++ components/mitsubishi_uart/mitsubishi_uart.cpp
@@ -3,35 +3,52 @@
 namespace esphome {
 namespace mitsubishi_uart {
 
-static const char *TAG = "mitsubishi_uart";
+const char *TAG = "mitsubishi_uart";
+
+const Packet PACKET_CONNECT = Packet(BYTE_PKTTYPE_CONNECT, 2)
+  .setPayloadByte(0,0xca).setPayloadByte(1,0x01);
 
 ////
 // Packet
 ////
-Packet::Packet(uint8_t packetType, uint8_t payloadSize) :
-  length{payloadSize + HEADER_SIZE + 1},
-  checksumIndex{length-1},
-  packetBytes{new uint8_t[length]()}
-{
+Packet::Packet(uint8_t packet_type, uint8_t payload_size)
+    : length{payload_size + HEADER_SIZE + 1}, checksumIndex{length - 1} {
   memcpy(packetBytes,EMPTY_PACKET,length);
-  packetBytes[HEADER_INDEX_PACKET_TYPE] = packetType;
-  packetBytes[HEADER_INDEX_PAYLOAD_SIZE] = payloadSize;
+  packetBytes[HEADER_INDEX_PACKET_TYPE] = packet_type;
+  packetBytes[HEADER_INDEX_PAYLOAD_SIZE] = payload_size;
 
   updateChecksum();
 }
 
-void Packet::updateChecksum() {
+Packet::Packet(uint8_t packet_header[HEADER_SIZE], uint8_t payload[], uint8_t payload_size, uint8_t checksum)
+    : length{payload_size + HEADER_SIZE + 1}, checksumIndex{length - 1} {
+  memcpy(packetBytes,packet_header,HEADER_SIZE);
+  memcpy(packetBytes+HEADER_SIZE, payload, payload_size);
+  packetBytes[checksumIndex] = checksum;
+}
+
+const uint8_t Packet::calculateChecksum() {
   uint8_t sum = 0;
   for (int i = 0; i < checksumIndex; i++) {
     sum += packetBytes[i];
   }
 
-  packetBytes[checksumIndex] = (0xfc - sum) & 0xff;
+  return (0xfc - sum) & 0xff;
 }
 
-void Packet::setPayloadByte(int payloadByteIndex, uint8_t value) {
-  packetBytes[HEADER_SIZE + payloadByteIndex] = value;
+Packet& Packet::updateChecksum() {
+  packetBytes[checksumIndex] = calculateChecksum();
+  return *this;
+}
+
+const bool Packet::isChecksumValid() {
+  return packetBytes[checksumIndex] == calculateChecksum();
+}
+
+Packet &Packet::setPayloadByte(int payload_byte_index, uint8_t value) {
+  packetBytes[HEADER_SIZE + payload_byte_index] = value;
   updateChecksum();
+  return *this;
 }
 
 // Packet PACKET_CONNECT = []() -> Packet {
@@ -45,8 +62,10 @@ void Packet::setPayloadByte(int payloadByteIndex, uint8_t value) {
 // MitsubishiUART
 ////
 
-MitsubishiUART::MitsubishiUART(uart::UARTComponent *uart_comp) {
-    this->_hp_uart = uart_comp;
+MitsubishiUART::MitsubishiUART(uart::UARTComponent *uart_comp) :
+  hp_uart{uart_comp}
+{
+    //this->_hp_uart = uart_comp;
 }
 
 void MitsubishiUART::setup() {
@@ -60,6 +79,7 @@ void MitsubishiUART::control(const climate::ClimateCall &call) {
 }
 
 void MitsubishiUART::update() {
+  readPackets();
   connect();
   ESP_LOGI(TAG, "Update!");
 }
@@ -69,10 +89,35 @@ void MitsubishiUART::dump_config() {
 }
 
 void MitsubishiUART::connect() {
-  Packet conn_packet = Packet(PKTTYPE_CONNECT,2);
-  conn_packet.setPayloadByte(0,0xca);
-  conn_packet.setPayloadByte(1,0x01);
-  _hp_uart->write_array(conn_packet.getBytes(),conn_packet.getLength());
+  sendPacket(PACKET_CONNECT);
+}
+
+void MitsubishiUART::sendPacket(Packet packet) {
+  hp_uart->write_array(packet.getBytes(),packet.getLength());
+}
+
+void MitsubishiUART::readPackets() {
+  uint8_t p_byte;
+  bool foundPacket = false;
+  while (hp_uart->available() && hp_uart->peek_byte(&p_byte)){
+    if (p_byte == BYTE_CONTROL){
+      foundPacket=true; 
+      ESP_LOGD(TAG, "FoundPacket!");
+      break;}
+  }
+
+  if (foundPacket && hp_uart->available() > HEADER_SIZE) {
+    uint8_t p_header[HEADER_SIZE];
+    hp_uart->read_array(p_header, HEADER_SIZE);
+    int payloadSize = p_header[HEADER_INDEX_PAYLOAD_SIZE];
+    uint8_t p_payload[payloadSize];
+    uint8_t checksum;
+    hp_uart->read_array(p_payload,payloadSize);
+    hp_uart->read_byte(&checksum);
+
+    Packet packet = Packet(p_header,p_payload,payloadSize,checksum);
+    ESP_LOGD(TAG, "Packet with type %x and %s validity.", packet.getType(), YESNO(packet.isChecksumValid()));
+  }
 }
 
 }  // namespace mitsubishi_uart
--- components/mitsubishi_uart/mitsubishi_uart.h
+++ components/mitsubishi_uart/mitsubishi_uart.h
@@ -7,30 +7,44 @@
 namespace esphome {
 namespace mitsubishi_uart {
 
-static const int HEADER_SIZE = 5;
-static const int HEADER_INDEX_PACKET_TYPE = 1;
-static const int HEADER_INDEX_PAYLOAD_SIZE = 4;
+const int PACKET_MAX_SIZE = 22; // Used to intialize blank packets
 
-static const uint8_t EMPTY_PACKET[22] = {0xfc, // Sync
+const int HEADER_SIZE = 5;
+const int HEADER_INDEX_PACKET_TYPE = 1;
+const int HEADER_INDEX_PAYLOAD_SIZE = 4;
+
+const uint8_t BYTE_CONTROL = 0xfc;
+
+const uint8_t EMPTY_PACKET[PACKET_MAX_SIZE] = {BYTE_CONTROL, // Sync
 0x00, // Packet type
 0x01, 0x30, // Unknown
 0x00, // Payload Size
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // Payload
 0x00}; // Checksum
 
-static const uint8_t PKTTYPE_CONNECT = 0x5a;
+
+
+const uint8_t BYTE_PKTTYPE_CONNECT = 0x5a;
 
 class Packet {
   public:
-    Packet(uint8_t packetType, uint8_t payloadSize);
-    const uint8_t* getBytes() {return packetBytes;};
+    // Constructors
+    Packet(uint8_t packet_type, uint8_t payload_size); // For building packets
+    Packet(uint8_t packet_header[HEADER_SIZE], uint8_t payload[], uint8_t payload_size, uint8_t checksum); // For reading packets
+    const uint8_t *getBytes() {return packetBytes;}; // Primarily for sending packets
     const int getLength() {return length;};
-    void setPayloadByte(int payloadByteIndex, uint8_t value);
+    Packet &setPayloadByte(int payload_byte_index, uint8_t value);
+    const bool isChecksumValid();
+
+    // Packet information getters
+    const uint8_t getType(){return packetBytes[HEADER_INDEX_PACKET_TYPE];};
   private:
     const int length;
     const int checksumIndex;
-    uint8_t* packetBytes;
-    void updateChecksum();
+    uint8_t packetBytes[PACKET_MAX_SIZE]{};
+    const uint8_t calculateChecksum();
+    Packet &updateChecksum();
+    
 };
 
 class MitsubishiUART : public climate::Climate, public PollingComponent {
@@ -55,9 +69,11 @@ class MitsubishiUART : public climate::Climate, public PollingComponent {
   void dump_config() override;
 
  private:
-  uart::UARTComponent *_hp_uart {nullptr};
+  uart::UARTComponent *hp_uart;
   climate::ClimateTraits _traits;
   void connect();
+  void sendPacket(Packet packet);
+  void readPackets();
 };
 
 }  // namespace mitsubishi_uart
